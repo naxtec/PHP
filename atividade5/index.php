06b42d5b2f0186edb5458f3d185366efd50ddc03
Rafael Ribeiro <rj.rafael99@gmail.com>
2025-10-22 19:09:18 -0300

    Alteração

--- atividade5/index.php
+++ atividade5/index.php
@@ -23,7 +23,7 @@
 </head>
 <body>
     <header>
-        <h1>Explorando o Mundo da Moda  </h1>
+        <h1>Explorando o Mundo da Moda , Venda de Roupas </h1>
     </header>   
     
     <section id="Mundo-moda">
